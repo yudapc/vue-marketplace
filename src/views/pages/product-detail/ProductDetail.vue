6986a6f3ec08601b2b6ccd37fc88658619ae1f07
Yuda Cogati <yudapc@gmail.com>
2020-07-27 09:24:42 +0700

    add product detail

--- src/views/pages/product-detail/ProductDetail.vue
+++ src/views/pages/product-detail/ProductDetail.vue
@@ -1,34 +1,124 @@
 <template>
-  <div>
-    <h1>{{ dataProduct.name }}</h1>
-    <div class="content">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore nihil veniam quaerat similique maxime, consequatur veritatis ullam maiores consequuntur aperiam exercitationem asperiores dolore nemo quae eum autem quos libero illum. Ducimus, accusamus a. Numquam error quaerat earum veniam nesciunt qui distinctio, libero voluptates fugiat, ipsa, perferendis fugit. Ab delectus facere ducimus vel at eum dolores rem magni, tenetur minus sit maiores aliquam vitae eveniet, alias reiciendis! Soluta rem nam minima eligendi commodi quidem dolor quod alias asperiores veniam quam, molestias exercitationem sit repellat nihil aperiam velit. Harum aliquid quibusdam, magni consequuntur mollitia ullam laudantium. Blanditiis minus incidunt ratione necessitatibus sequi nisi sit dicta nam assumenda, quis possimus dolor, architecto corrupti ullam velit labore sint aliquam quidem voluptatibus repellendus? Dolorem eum temporibus autem quis velit dicta magni iure itaque laudantium. A et error dolor, atque sequi dolores. Cumque accusamus minima quasi fuga similique adipisci maiores aspernatur eum accusantium ratione. Mollitia fuga repellendus hic suscipit corporis molestias voluptates? Commodi labore quam enim delectus vitae? Libero tempora nemo, harum ratione repellat natus, doloremque, quidem molestiae corrupti necessitatibus assumenda voluptate esse quibusdam! Neque vero animi consequatur illum harum at tenetur maxime magnam quasi? Molestiae quo architecto distinctio explicabo quia id doloremque quis culpa officiis commodi, dolor dolorem voluptas excepturi perferendis ratione, sequi sunt. Aut libero, obcaecati amet ratione incidunt at soluta minus vitae perferendis nihil in blanditiis possimus. Perspiciatis doloremque ipsa impedit odio voluptatem non fugiat voluptatibus veniam earum similique? Accusantium modi veniam quasi ipsam vero unde voluptas maxime nulla? Assumenda, asperiores eius earum saepe in iure veritatis temporibus eos minima voluptates repellendus amet iusto harum minus illo alias, cum, vitae nesciunt magnam voluptatibus molestiae veniam sequi hic nostrum? Culpa quae aliquid incidunt architecto et quo non. At, quibusdam id est iure illum ipsa voluptatibus hic placeat ad perspiciatis atque autem exercitationem. Esse molestias reprehenderit eveniet placeat in architecto sunt dolores, adipisci, eos deleniti aut unde soluta voluptate officiis, est nesciunt quia odio recusandae quibusdam eum consectetur possimus repellendus ab voluptas. Nihil illo architecto ipsum, iure quis culpa voluptatibus officia. Veritatis quas optio excepturi fugiat dolorum, nobis at voluptate, quos, possimus ipsam aliquam nostrum! Voluptatibus perspiciatis incidunt iure nisi autem culpa saepe omnis, tempora asperiores recusandae? Commodi voluptates molestiae facere vitae voluptatum ad voluptatem ex. Eum incidunt odit quibusdam maiores officiis assumenda velit perferendis, ea illum non ipsa, optio accusantium laudantium quaerat iure commodi animi, sit illo repellat. Vel consectetur fugiat magni impedit explicabo totam harum necessitatibus inventore dolor, reiciendis quasi? Culpa numquam reprehenderit ratione voluptatum amet voluptate adipisci vel fuga odio, itaque repellendus ducimus nihil laboriosam veritatis, labore praesentium atque vitae, maxime nostrum placeat blanditiis? Tempora quod numquam, deserunt esse molestias similique cupiditate voluptatibus sint doloribus reprehenderit sunt illum culpa quam quis excepturi iure. Perferendis nulla id harum labore unde illum, voluptates corporis molestiae, dolor excepturi eum quasi, in eligendi. Nobis, cum omnis ducimus culpa odit esse dolorem molestias maxime soluta ipsa beatae. Harum repellendus quasi eum assumenda vitae, sapiente fugit quam deserunt ullam molestiae, architecto quos ad maiores libero saepe facere! Ab.</div>
+  <div class="product-detail-container">
+    <div class="productImages">
+      <ProductImages :images="dataProduct.images" />
+    </div>
+    <div class="content">
+      <h1 class="product-name">{{ dataProduct.name }}</h1>
+      <div class="description border-top">
+        {{ dataProduct.description }}</div>
+      <div class="context-section border-top">
+        <span class="section-item section-label">HARGA</span>
+        <span class="section-item price">{{ rupiah(dataProduct.original_price) }}</span>
+      </div>
+      <div class="context-section border-top">
+        <span class="section-item section-label">JUMLAH</span>
+        <span class="section-item"></span>
+      </div>
+       <div class="context-section border-top">
+        <span class="section-item section-label">PROMO</span>
+        <span class="section-item"></span>
+      </div>
+       <div class="context-section border-top">
+        <span class="section-item section-label">INFO PRODUK</span>
+        <span class="section-item"></span>
+      </div>
+       <div class="context-section border-top">
+        <span class="section-item section-label">ONGKOS KIRIM</span>
+        <span class="section-item"></span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
+import { rupiah } from "../../../helpers/rupiah";
+import ProductImages from "../../../components/ProductImages";
 
 export default {
-  name: 'ProductDetail',
+  name: "ProductDetail",
+  components: {
+    ProductImages,
+  },
   data() {
     return {
-      dataProduct: {}
-    }
+      dataProduct: {},
+      rupiah,
+    };
   },
   methods: {
     async getProductDetail(id) {
       try {
-        const response = await axios.get("https://dev-api.bisnisblanja.my.id/api/v1/catalog/products-new/" + id);
+        const response = await axios.get(
+          "https://dev-api.bisnisblanja.my.id/api/v1/catalog/products-new/" + id
+        );
         const dataUIComponent = response.data;
         this.dataProduct = dataUIComponent;
       } catch (error) {
-        console.log('Errornya: ', error);
+        console.log("Errornya: ", error);
       }
-    }
+    },
   },
   mounted() {
     const id = this.$route.params.id;
     this.getProductDetail(id);
-  }
-}
+  },
+};
 </script>
+
+<style scoped>
+.product-detail-container {
+  display: grid;
+  grid-template-columns: 0.5fr 1fr;
+  grid-template-areas:
+    "productImages content";
+  gap: 15px;
+  justify-content: center;
+  margin: 50px auto;
+  width: 70%;
+}
+.productImages {
+  grid-area: productImages;
+}
+.content {
+  grid-area: content;
+}
+
+.product-name {
+  font-weight: 800;
+  font-size: 1.25rem;
+}
+
+.border-top {
+  padding: 20px 0px;
+  border-top: 1px solid rgb(229, 231, 233);
+}
+.context-section {
+  display: flex;
+  text-align: center;
+  justify-content:flex-start
+}
+.price {
+  color: rgb(250, 89, 29);
+  line-height: 30px;
+  font-size: 24px;
+  margin: 0px;
+  font-weight: bold;
+}
+.section-item {
+  width: 200px;
+}
+.section-label {
+  text-align: left;
+  display: block;
+  position: relative;
+  font-weight: 700;
+  font-size: 0.857143rem;
+  line-height: 18px;
+  color: rgba(49, 53, 59, 0.32);
+  text-decoration: initial;
+  margin: 0px;
+}
+
+</style>
